6efb89f2b0032f5e5f185224e631380ef1d7bb3d
Baatyrbek <batrudinovshapiro@gmail.com>
2025-09-13 04:42:01 -0500

    custom object train

--- my3dprjct/src/main.js
+++ my3dprjct/src/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'; //Custom obj loader
 
 // Scene
 const scene = new THREE.Scene();
@@ -13,8 +14,8 @@ const renderer = new THREE.WebGLRenderer({
 // Camera
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize( window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
-camera.position.setX(-3);
+camera.position.setZ(0);
+camera.position.setX(0);
 
 renderer.render(scene, camera);
 
@@ -28,8 +29,8 @@ scene.add(torus)
 
 
 //Light
-const pointLight = new THREE.PointLight(0xffffff)
-pointLight.position.set(5,5,5)
+const pointLight = new THREE.PointLight(0xffffff, 3000)
+pointLight.position.set(10,20,30)
 
 const ambietLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambietLight)
@@ -50,19 +51,19 @@ Array(200).fill().forEach(addStar)
 
 
 //Space texture
-const spaceTexture = new THREE.TextureLoader().load('../space.jpg');
+const spaceTexture = new THREE.TextureLoader().load('../obj/space.jpg');
 scene.background = spaceTexture;
 
 
 //CubeLogo
-const myLogoTexture = new THREE.TextureLoader().load('../aa.jpg');
+const myLogoTexture = new THREE.TextureLoader().load('../obj/aa.jpg');
 const myLogo = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: myLogoTexture }));
 scene.add(myLogo);
 
 
 //Moon
-const moonTexture = new THREE.TextureLoader().load('../moon.jpg')
-const normalTexture = new THREE.TextureLoader().load('../normal.jpg')
+const moonTexture = new THREE.TextureLoader().load('../obj/moon.jpg')
+const normalTexture = new THREE.TextureLoader().load('../obj/normal.jpg')
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3,32,32),
   new THREE.MeshStandardMaterial({
@@ -73,15 +74,44 @@ const moon = new THREE.Mesh(
 );
 scene.add(moon);
 
-moon.position.z = 30;
+moon.position.z = 20;
 moon.position.setX(-10);
 myLogo.position.z = -5;
 myLogo.position.x = 2;
 
 
+//Custom obj
+const loader = new GLTFLoader();
+let model;
+loader.load(
+  '../obj/bocchi_the_rock.glb',  // Path to your .glb file
+  function (gltf) {
+    model = gltf.scene;
+    scene.add(model);
+    model.position.z = 30;
+    model.position.setX(10);
+  }
+);
+
+// //Kars obj
+// const loaderrrr = new GLTFLoader();
+// let modelkar;
+// loaderrrr.load(
+//   '../obj/kars.glb',  // Path to your .glb file
+//   function (gltf) {
+//     modelkar = gltf.scene;
+//     scene.add(modelkar);
+//     model.position.z = 35;
+//     model.position.setX(10);
+//     mesh.scale.set(2,2,2);
+//   }
+// );
+
 //Scroll Camera Movement
 function moveCamera(){
   const t = document.body.getBoundingClientRect().top;
+
+
   moon.rotation.x += 0.05;
   moon.rotation.y += 0.075;
   moon.rotation.z += 0.05;
@@ -97,7 +127,6 @@ function moveCamera(){
 document.body.onscroll = moveCamera;
 moveCamera();
 
-
 //Torus animation, renderer initializer 
 function animate(){
   requestAnimationFrame(animate);
@@ -108,18 +137,19 @@ function animate(){
   
   moon.rotation.x += 0.005;
 
-  // controls.update();
+  model.rotation.x += 0.005;
+  model.rotation.y += 0.0075;
+  model.rotation.z += 0.005;
+
+  controls.update();
   
   renderer.render(scene, camera);
+  
 }
-animate()
-
-
 //Helpers
 const lightHelper = new THREE.PointLightHelper(pointLight)
-const lightHelper2 = new THREE.PointLightHelper(light)
 const gridHelper = new THREE.GridHelper(200,50)
-scene.add(lightHelper, gridHelper, lightHelper2)
-
+scene.add(lightHelper, gridHelper)
 const controls = new OrbitControls(camera, renderer.domElement);
+animate()
 
